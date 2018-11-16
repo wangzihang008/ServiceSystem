50c4f329a66c6255bfd186e22c79d06c9917816a
trainee1 <trainee1@LAPSHA00010.fdmgroup.local>
2018-11-16 10:16:28 +0800

    Modified test file - LoginControllerTest

--- ServiceSystem/src/main/test/com/fdmgroup/Controller/LoginControllerTest.java
+++ ServiceSystem/src/main/test/com/fdmgroup/Controller/LoginControllerTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import com.fdmgroup.Entity.Customer;
+import com.fdmgroup.Entity.Vendor;
 import com.fdmgroup.Entity.Dao.CustomerDao;
 
 public class LoginControllerTest {
@@ -24,6 +25,14 @@ public class LoginControllerTest {
 	@Before
 	public void startInjectionMock() {
 		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void When_goToLogin_Given_model_Then_returnLoginJspPage() {
+		Model model = mock(Model.class);
+		String nextPage = lc.goToLogin(model);
+		
+		assertEquals("login", nextPage);
 	}
 	
 	@Test
@@ -49,17 +58,35 @@ public class LoginControllerTest {
 	}
 	
 	@Test
-	public void When_goToLogin_Given_customerUsernameNotEmpty_Then_returnLoginJspPage() {
+	public void When_goToLogin_Given_customerUsernameAndPassword_Then_returnLoginJspPage() {
 		Model model = mock(Model.class);
 		Customer customer = new Customer();
 		customer.setUsername("robert");
-		customer.setUsername("robert");
+		customer.setPassword("robert");
 		HttpSession hs = mock(HttpSession.class);
 		
 		when(cDao.getByUsernameAndPassword(customer.getUsername(), customer.getPassword())).thenReturn(customer);
 		
 		String nextPage = lc.goToLogin(model, customer, hs);
 		
-		assertEquals("login", nextPage);
+		assertEquals("dashboard", nextPage);
+	}
+	
+	@Test
+	public void When_goToLogin_Given_customerUsernameAndPasswordAndVendor_Then_returnLoginJspPage() {
+		Model model = mock(Model.class);
+		Customer customer = new Customer();
+		customer.setUsername("robert");
+		customer.setPassword("robert");
+		Vendor vendor = new Vendor();
+		customer.setVendor(vendor);
+		
+		HttpSession hs = mock(HttpSession.class);
+		
+		when(cDao.getByUsernameAndPassword(customer.getUsername(), customer.getPassword())).thenReturn(customer);
+		
+		String nextPage = lc.goToLogin(model, customer, hs);
+		
+		assertEquals("dashboard", nextPage);
 	}
 }
